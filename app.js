653eaf2bfd317761eba94501f2cbc362b5aed604
lucastonelotto-artk <lucas.tonelotto@artekium.com>
2025-08-27 10:57:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 // Configs
 const port = process.env.PORT || 3000;
 const verifyToken = "787052530498023";
-const rasaURL = "https://93404610b58e.ngrok-fr/webhooks/rest/webhook`";
+const rasaURL = "https://7138486d345d.ngrok-fr/webhooks/rest/webhook`";
 const whatsappToken = "EAASx3p5EdCMBPXpEAFBVFKKhthJFJ0qIUmY0EPx3vwJN2vi0NeZAvjadVwMdZAyfL0qNFIbV8ZCgYOapHxYZAmiB4dQAiaJ5CBTAQKoMAhN60Rv0G9i0mMpsjlEy7b1Ki45gqAGFnBrJDAbvO9SuF2LB8yjqTG7RW6HZAmHQiyQtF1nTcUhIZBZAeSObH4hMnCb9w5hQiXc82ZCCD074DVoFIFLRYylZBILU5d1TKmyghzKd0vDiawTA0P831bwZDZD";
 const phoneNumberId = "819298601257509";
 
